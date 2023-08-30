d8d5c666c96f1b416a185a8ea3511e347b746c34
Connors <brendan.connors@dfo-mpo.gc.ca>
2023-08-29 23:01:31 -0700

    Update functions.R

--- functions.R
+++ functions.R
@@ -27,7 +27,7 @@ geographic.order <- function(x) {
   
   WC_stk$geo_id <- data.table::frankv(WC_stk, cols=wc.ind, ties.method = "first")
   SEAK_stk$geo_id <- nrow(SEAK_stk) + data.table::frankv(SEAK_stk, cols=seak.ind, order=-1L, ties.method = "first")
-  GOA_stk$geo_id <- nrow(WC_stk) + nrow(SEAK_stk) data.table::frankv(GOA_stk, cols=goa.ind, order=-1L, ties.method = "first")
+  GOA_stk$geo_id <- nrow(WC_stk) + nrow(SEAK_stk) + data.table::frankv(GOA_stk, cols=goa.ind, order=-1L, ties.method = "first")
   BS_stk$geo_id <- nrow(WC_stk) + nrow(SEAK_stk) + nrow(GOA_stk) + data.table::frankv(BS_stk, cols=bs.ind, ties.method="first")
   
   #combine them
