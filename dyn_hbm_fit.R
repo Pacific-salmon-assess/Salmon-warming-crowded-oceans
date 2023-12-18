7e7e07880e4a90ca6dd88ab11b0f64db3a217afe
hvhunter <hannah.hunter@dfo-mpo.gc.ca>
2023-12-18 09:15:57 -0700

    Remove code to run single covariate model versions

--- dyn_hbm_fit.R
+++ dyn_hbm_fit.R
@@ -1,12 +1,7 @@
 ## Fit Nonstationary hierarchical Bayesian models ("Era" and Random walk)
 # This is adapted from: https://github.com/michaelmalick/sockeye-nonstationary
 
-
-## Set Species -----
-speciesFlag = "pink"
-#speciesFlag = "chum"
-#speciesFlag = "sockeye"
-
+# Species
 if(speciesFlag=="pink") 
   data_master <- pink else if(speciesFlag=="chum") 
     data_master <- chum else if(speciesFlag=="sockeye")
@@ -30,17 +25,6 @@ if(!dir.exists(diag.dir))
 
 
 ## Get data for Stan ---------------------------------------
-stan.dat.sst  <- stan_data_dyn(data_master, "early_sst_stnd",
-                           breakpoint1 = 1977,
-                           breakpoint2 = 1989,
-                           var.region = "Ocean.Region2",
-                           scale.x1 = TRUE)
-
-stan.dat.comp <- stan_data_dyn(data_master, "np_pinks_sec_stnd",
-                               breakpoint1 = 1977,
-                               breakpoint2 = 1989,
-                               var.region = "Ocean.Region2",
-                               scale.x1 = TRUE)
 
 stan.dat.2c <- stan_data_dyn(data_master, 
                              var.x2 = "early_sst_stnd",
@@ -48,16 +32,12 @@ stan.dat.2c <- stan_data_dyn(data_master,
                              breakpoint1 = 1977,
                              breakpoint2 = 1989,
                              var.region="Ocean.Region2", 
-                             scale.x1 = TRUE)
+                             scale.x1 = TRUE,
+                             alpha.group = FALSE)
 
 
 ## Set pars to monitor -------------------------------------
 
-pars_era_1c <- c("alpha", "beta", "sigma", "phi", "mu_alpha", "sigma_alpha",
-                 "gamma1", "gamma2", "gamma3",
-                 "mu_gamma1", "mu_gamma2", "mu_gamma3", "sigma_gamma")
-pars_dyn_1c <- c("alpha", "beta", "sigma", "phi", "mu_alpha", "sigma_alpha",
-                 "gamma", "sigma_gamma", "signal_noise")
 pars_era_2c <- c("alpha", "beta", "sigma", "phi", "mu_alpha", "sigma_alpha",
                  "gamma1", "gamma2", "gamma3", 
                  "mu_gamma1", "mu_gamma2", "mu_gamma3", "sigma_gamma", 
@@ -70,8 +50,6 @@ pars_dyn_2c <- c("alpha", "beta", "sigma", "phi", "mu_alpha", "sigma_alpha",
 pars.gen.quant <- c("log_lik", "yrep") ## Generated quantities to monitor
 
 
-save(pars_era_1c, file = "./output/pars_era_1c.RData")
-save(pars_dyn_1c, file = "./output/pars_dyn_1c.RData")
 save(pars_era_2c, file = "./output/pars_era_2c.RData")
 save(pars_dyn_2c, file = "./output/pars_dyn_2c.RData")
 
@@ -80,60 +58,6 @@ save(pars.gen.quant, file = "./output/pars_gen_quant.RData")
 
 
 ## Run MCMC  -----------------------------------------
-
-## SST
-
-era.sst <- rstan::stan(file = "./stan/hbm_era_1c.stan",
-                        data = stan.dat.sst,
-                        pars = c(pars_era_1c, pars.gen.quant),
-                        warmup = 1000,
-                        iter = 2000,
-                        cores = 4,
-                        chains = 4,
-                        seed = 123,
-                        control = list(adapt_delta = 0.99,
-                                       max_treedepth = 20))
-save(era.sst, file = here(fit.dir, "hbm_era_sst.RData"))
-
-dyn.sst <- rstan::stan(file = "./stan/hbm_dyn_1c.stan",
-                        data = stan.dat.sst,
-                        pars = c(pars_dyn_1c, pars.gen.quant),
-                        warmup = 1000,
-                        iter = 2000,
-                        cores = 4,
-                        chains = 4,
-                        seed = 123,
-                        control = list(adapt_delta = 0.99,
-                                       max_treedepth = 20))
-save(dyn.sst, file = here(fit.dir, "hbm_dyn_sst.RData"))
-
-
-## Comp
-
-era.comp <- rstan::stan(file = "./stan/hbm_era_1c.stan",
-                        data = stan.dat.comp,
-                        pars = c(pars_era_1c, pars.gen.quant),
-                        warmup = 1000,
-                        iter = 2000,
-                        cores = 4,
-                        chains = 4,
-                        seed = 123,
-                        control = list(adapt_delta = 0.99,
-                                       max_treedepth = 20))
-save(era.comp, file = here(fit.dir, "hbm_era_comp.RData"))
-
-dyn.comp <- rstan::stan(file = "./stan/hbm_dyn_1c.stan",
-                        data = stan.dat.comp,
-                        pars = c(pars_dyn_1c, pars.gen.quant),
-                        warmup = 1000,
-                        iter = 2000,
-                        cores = 4,
-                        chains = 4,
-                        seed = 123,
-                        control = list(adapt_delta = 0.99,
-                                       max_treedepth = 20))
-save(dyn.comp, file = here(fit.dir, "hbm_dyn_comp.RData"))
-
 
 ## 2-covariate
 
@@ -153,7 +77,7 @@ dyn.2c <- rstan::stan(file = "./stan/hbm_dyn_2c.stan",
                       data = stan.dat.2c,
                       pars = c(pars_dyn_2c, pars.gen.quant),
                       warmup = 1000,
-                      iter = 2000,
+                      iter = 4000, # try more iterations to fix Rhat & Neff
                       cores = 4,
                       chains = 4,
                       seed = 123,
@@ -161,137 +85,5 @@ dyn.2c <- rstan::stan(file = "./stan/hbm_dyn_2c.stan",
                                      max_treedepth = 20))
 save(dyn.2c, file = here(fit.dir, "hbm_dyn_2c.Rdata"))
 
-## Check pathology -----------------------------------------
-# does not include 2-covar models yet
 
-## NUTS diag
-
-rstan::check_hmc_diagnostics(era.sst)
-rstan::check_hmc_diagnostics(dyn.sst)
-
-rstan::check_hmc_diagnostics(era.comp)
-rstan::check_hmc_diagnostics(dyn.comp)
-
-## Neff
-
-neff_lowest(era.sst, pars = pars_era_1c)
-neff_lowest(dyn.sst, pars = pars_dyn_1c)
-
-
-neff_lowest(era.comp, pars = pars_era_1c)
-neff_lowest(dyn.comp, pars = pars_dyn_1c)
-
-## Rhat
-
-rhat_highest(era.sst, pars = pars_era_1c)
-rhat_highest(dyn.sst, pars = pars_dyn_1c)
-
-rhat_highest(era.comp, pars = pars_era_1c)
-rhat_highest(dyn.comp, pars = pars_dyn_1c)
-
-
-## Pairs
-#
-# pairs_lowest(era.sst, pars = pars_era_c1)
-# pairs_lowest(dyn.sst, pars = pars_dyn_c1)
-## pairs_lowest(era.comp, pars = pars_era_c1)
-# pairs_lowest(dyn.comp, pars = pars_dyn_c1)
-
-## Run time
-
-rstan::get_elapsed_time(era.sst)
-rstan::get_elapsed_time(dyn.sst)
-
-rstan::get_elapsed_time(era.comp)
-rstan::get_elapsed_time(dyn.comp)
-
-
-## MCMC diagnostics ----------------------------------------
-
-## SST
-
-pdf(here(fig.dir, "era_sst_diag.pdf"), width = 7, height = 5)
-    coda_neff(get_neff(era.sst, pars = pars_era_1c), total_draws(era.sst))
-    coda_rhat(get_rhat(era.sst, pars = pars_era_1c))
-    coda_diag(As.mcmc.list(era.sst, pars = pars_era_1c))
-dev.off()
-
-
-pdf(here(fig.dir, "dyn_sst_diag.pdf"), width = 7, height = 5)
-    coda_neff(get_neff(dyn.sst, pars = pars_dyn_1c), total_draws(dyn.sst))
-    coda_rhat(get_rhat(dyn.sst, pars = pars_dyn_1c))
-    coda_diag(As.mcmc.list(dyn.sst, pars = pars_dyn_1c))
-dev.off()
-
-## Comp
-
-pdf(here(fig.dir, "era_comp_diag.pdf"), width = 7, height = 5)
-  coda_neff(get_neff(era.comp, pars = pars_era_1c), total_draws(era.comp))
-  coda_rhat(get_rhat(era.comp, pars = pars_era_1c))
-  coda_diag(As.mcmc.list(era.comp, pars = pars_era_1c))
-dev.off()
-
-
-pdf(here(fig.dir, "dyn_comp_diag.pdf"), width = 7, height = 5)
-  coda_neff(get_neff(dyn.comp, pars = pars_dyn_1c), total_draws(dyn.comp))
-  coda_rhat(get_rhat(dyn.comp, pars = pars_dyn_1c))
-  coda_diag(As.mcmc.list(dyn.comp, pars = pars_dyn_1c))
-dev.off()
-
-
-## Posterior predictive checks -----------------------------
-plot_post_pc(era.sst, stan.dat.sst$y, pdf.path = here(fig.dir, "era_sst_yrep.pdf"))
-plot_post_pc(dyn.sst, stan.dat.sst$y, pdf.path = here(fig.dir, "dyn_sst_yrep.pdf"))
-
-plot_post_pc(era.comp, stan.dat.comp$y, pdf.path = here(fig.dir, "era_comp_yrep.pdf"))
-plot_post_pc(dyn.comp, stan.dat.comp$y, pdf.path = here(fig.dir, "dyn_comp_yrep.pdf"))
-
-
-
-## LOOIC ---------------------------------------------------
-
-loo.era.sst <- rstan::loo(era.sst, cores = 4)
-loo.dyn.sst <- rstan::loo(dyn.sst, cores = 4)
-
-
-loo.era.comp <- rstan::loo(era.comp, cores = 4)
-loo.dyn.comp <- rstan::loo(dyn.comp, cores = 4)
-
-
-
-save(loo.era.sst, file = here(diag.dir, "loo_era_sst.RData"))
-save(loo.dyn.sst, file = here(diag.dir, "loo_dyn_sst.RData"))
-
-save(loo.era.comp, file = here(diag.dir, "loo_era_comp.RData"))
-save(loo.dyn.comp, file = here(diag.dir, "loo_dyn_comp.RData"))
-
-
-
-sum(pareto_k_values(loo.era.sst) > 0.7)
-sum(pareto_k_values(loo.dyn.sst) > 0.7)
-
-sum(pareto_k_values(loo.era.comp) > 0.7)
-sum(pareto_k_values(loo.dyn.comp) > 0.7)
-
-
-
-## SST
-
-pdf(here(fig.dir, "era_sst_loo.pdf"), width = 7, height = 5)
-    plot(loo.era.sst, label_points = TRUE)
-dev.off()
-
-pdf(here(fig.dir, "dyn_sst_loo.pdf"), width = 7, height = 5)
-    plot(loo.dyn.sst, label_points = TRUE)
-dev.off()
-
-## Comp
-
-pdf(here(fig.dir, "era_comp_loo.pdf"), width = 7, height = 5)
-plot(loo.era.comp, label_points = TRUE)
-dev.off()
-
-pdf(here("dyn_comp_loo.pdf"), width = 7, height = 5)
-plot(loo.dyn.comp, label_points = TRUE)
-dev.off()
 
