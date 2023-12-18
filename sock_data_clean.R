ea4f7db5bcc0e1695996248c3aec984bd385979d
hvhunter <hannah.hunter@dfo-mpo.gc.ca>
2023-12-18 09:25:41 -0700

    Use full data compilation for Sockeye

--- sock_data_clean.R
+++ sock_data_clean.R
@@ -5,53 +5,71 @@
 ## This script cleans/processes the raw compiled sockeye data. The output of
 ## this script is a master brood table for the analysis and a summary info table
 
-
 ## Read in downloaded data
-s.brood <- read.table("./data-downloaded/sockeye/raw_brood_table_2023_07_19.csv",
-                      sep = ",",skip = 0,
-                      na.string = c("NA", "ND", "Canadians:  Please check the G-R ages.  "),
-                      stringsAsFactors = FALSE, header = TRUE)
+data_full <- read.csv("./data-downloaded/salmon_productivity_compilation2023-11-20.csv", row.names=1) 
+info_full <- read.csv("./data-downloaded/stock_info2023-11-20.csv", row.names=1)
 
-s.info <- read.table("./data-downloaded/sockeye/raw_stock_info_2023_06_18.csv", sep = ",",
-                     skip = 0, stringsAsFactors = FALSE, header = TRUE, quote="", fill=TRUE)
+## Filter for Sockeye
+s.brood <- data_full %>% dplyr::filter(species == "Sockeye")
+s.info <- info_full %>% dplyr::filter(species == "Sockeye")
 
 
 ## Clean-up master brood table -----------------------------
 head(s.brood)
 tail(s.brood)
 sapply(s.brood, class)
+summary(s.brood)
 
 # NA values need to be replaced with 0s for years with no recruits
 # This replacement is only done for the "recruit" columns
-r.cols <- grep("^R[[:digit:]]\\.[[:digit:]]", names(s.brood), value = TRUE)
-s.brood[ , r.cols][is.na(s.brood[ , r.cols])] <- 0
+r.cols.old <- grep("^r[[:digit:]]\\.[[:digit:]]", names(s.brood), value = TRUE)
+s.brood[ , r.cols.old][is.na(s.brood[ , r.cols.old])] <- 0
+if(!"R2.5" %in% names(s.brood)) {
+  s.brood$r2.5 <- 0 }
 head(s.brood)
 tail(s.brood)
 sapply(s.brood, class)
 
-## Subset usable data points
-s.brood.use <- s.brood[s.brood$UseFlag == 1, ]
-head(s.brood.use)
-tail(s.brood.use)
-nrow(s.brood.use)
-summary(s.brood.use)
+#Rename some columns
+names(s.brood) <- str_to_title(names(s.brood))
+names(s.brood)[names(s.brood) %in% c("Broodyear", "Stock.id")] <- c("BY", "Stock.ID")
+#names(s.brood)[names(s.brood) %in% c("Spawners", "Recruits")] <- c("S", "R")
+head(s.brood)
 
-names(s.brood.use)[names(s.brood.use) == "BroodYear"] <- "BY"
-if(!"R2.5" %in% names(s.brood)) {
-    s.brood.use$R2.5 <- 0
-}
+
+## Lat/Lon lookup table
+s.info$lon <- -abs(s.info$lon)
+coord.lookup <- distinct(s.info[,c("stock.name", "lat", "lon")])
+names(coord.lookup) <- str_to_title(names(coord.lookup))
+
+# join lat/lon
+all(s.brood$Stock %in% s.info$stock.name)
+all(s.brood$Stock.ID %in% s.info$stock.id)
+all(s.brood$Stock %in% coord.lookup$Stock)
+s.brood <- left_join(s.brood, coord.lookup, by=c("Stock"="Stock.name"))
+head(s.brood)
+
+
+# Shorten Stock names
+s.brood$Stock <- stringr::str_remove(s.brood$Stock, "-Sockeye")
+# temporarily remove stock with duplicated BYs:
+s.brood <- s.brood %>% filter(Stock!="Sustut")
 
 
 ## Create data frame with estimates of recruits, spawners and proportion of
 ## recruits that entered ocean at age 0, 1 and 2 and from each age class
  # Use detailed recruit info if it exists, and average proportions if not
+r.cols <- grep("^R[[:digit:]]\\.[[:digit:]]", names(s.brood), value = TRUE)
+
 source("recruitment_means.R")
 
-bt <- ddply(s.brood.use, c("Stock.ID", "BY"),function(x) {
-  cond <- x$DetailFlag==1
+bt <- ddply(s.brood, c("Stock.ID", "BY"),function(x) {
+  DetailFlag <- ifelse(all(x[r.cols]==0), 0, 1)
+  if(nrow(x) > 1) print("Duplicates present!", x) 
+  cond <- DetailFlag == 1
 	R <- if_else(cond, sum(x[ , grep("^R[[:digit:]]\\.[[:digit:]]", names(x))], na.rm = TRUE), 
-	             x$TotalRecruits) # keep total recruits as is if no detail, otherwise sum recruits
-	S <- x$TotalEscapement # spawners
+	             x$Recruits) # keep total recruits as is if no detail, otherwise sum recruits
+	S <- x$Spawners # spawners
 	ocean_0 <- ifelse(cond, sum(x[,grep("^R0\\.", names(x))],na.rm=T)/R, 
 	                  lifehist$prop_age0[match(x$Stock, lifehist$Stock)])
 	ocean_1 <- ifelse(cond, sum(x[,grep("^R1\\.", names(x))],na.rm=T)/R, 
@@ -62,39 +80,17 @@ bt <- ddply(s.brood.use, c("Stock.ID", "BY"),function(x) {
 	                  lifehist$prop_age3[match(x$Stock, lifehist$Stock)])
 	ocean_4 <- ifelse(cond, sum(x[,grep("^R4\\.", names(x))],na.rm=T)/R, 
 	                  lifehist$prop_age4[match(x$Stock, lifehist$Stock)])
-	DetailFlag <- x$DetailFlag # keep this flag in for now
-	R0.1 <- x[,"R0.1"]/R
-	R0.2 <- x[,"R0.2"]/R
-	R0.3 <- x[,"R0.3"]/R
-	R0.4 <- x[,"R0.4"]/R
-	R0.5 <- x[,"R0.5"]/R
-	R1.1 <- x[,"R1.1"]/R
-	R1.2 <- x[,"R1.2"]/R
-	R1.3 <- x[,"R1.3"]/R
-	R1.4 <- x[,"R1.4"]/R
-	R1.5 <- x[,"R1.5"]/R
-	R2.1 <- x[,"R2.1"]/R
-	R2.2 <- x[,"R2.2"]/R
-	R2.3 <- x[,"R2.3"]/R
-	R2.4 <- x[,"R2.4"]/R
-	R2.5 <- x[,"R2.5"]/R
-	R3.1 <- x[,"R3.1"]/R
-	R3.2 <- x[,"R3.2"]/R
-	R3.3 <- x[,"R3.3"]/R
-	R3.4 <- x[,"R3.4"]/R
-	R4.1 <- x[,"R4.1"]/R
-	R4.2 <- x[,"R4.2"]/R
-	R4.3 <- x[,"R4.3"]/R
-    data.frame(Stock = x$Stock,
-               Region = x$Region,
-               Ocean.Region = x$Ocean.Region,
-               Sub.Region = x$Sub.Region,
+    data.frame(Species="Sockeye",
+               Stock = x$Stock,
                Lat = x$Lat,
                Lon = x$Lon,
-               R, S, ocean_0, ocean_1, ocean_2, ocean_3, ocean_4, DetailFlag, R0.1, R0.2, R0.3, R0.4,
-               R0.5, R1.1, R1.2, R1.3, R1.4, R1.5, R2.1, R2.2, R2.3, R2.4, R2.5, R3.1,
-               R3.2, R3.3, R3.4, R4.1, R4.2, R4.3, stringsAsFactors = FALSE)
-	})
+               R, S, ocean_0, ocean_1, ocean_2, ocean_3, ocean_4, x$R0.1, x$R0.2, x$R0.3, x$R0.4, x$R0.5, x$R1.1, x$R1.2, x$R1.3, x$R1.4, x$R1.5, x$R2.1, x$R2.2, x$R2.3, x$R2.4, x$R2.5, x$R3.1, x$R3.2, x$R3.3, x$R3.4, x$R4.1, x$R4.2, x$R4.3, stringsAsFactors = FALSE)
+	}) 
+
+## Need to figure out what to do with new sockeye data in the above
+
+bt[,r.cols] <- lapply(bt[,r.cols], function(x) x/bt$R) # Make "R" columns proportions
+
 
 
 ## Check that all Rx.x columns were included
