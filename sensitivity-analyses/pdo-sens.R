27d864893f9960e9f543bc8269b0ad4dee4b715e
hvhunter <hannah.hunter@dfo-mpo.gc.ca>
2024-09-27 09:55:29 -0700

    PDO analysis

---
+++ sensitivity-analyses/pdo-sens.R
@@ -0,0 +1,19 @@
+## Sensitivity analysis of including PDO in models
+
+
+# Get PDO as an appropriately weighted covariate
+  # can use clim_wgt_avg for this?
+
+# Explore correlation between PDO and other covariates
+  # By region and by time period
+
+# Explore correlation between PDO and response (lnRS)
+  # by region and time period
+
+# Run stationary model with PDO covariate added
+
+# Run eras model with PDO covariate added
+# use 'hbm_era_3c.stan'
+
+# Compare residuals, run model comparisons
+
