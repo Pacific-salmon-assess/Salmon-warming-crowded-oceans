bb739687c7123a63b9909143bf42af0a8411b335
hvhunter <hannah.hunter@dfo-mpo.gc.ca>
2024-09-27 09:50:06 -0700

    Add scripts for nonlinearity plots and detrending

---
+++ detrend_covars.R
@@ -0,0 +1,358 @@
+## De-trend covariates
+
+if(!dir.exists("./figures/detrend-covars/"))
+    dir.create("./figures/detrend-covars/")
+
+## Pink abundance ------------------------------------------
+
+## Get pink abundance data
+comp.dat <- read.table(file = "./data-downloaded/competitor_indices_2024.csv",
+                       stringsAsFactors = FALSE, sep = ",",
+                       header=TRUE)
+comp.dat <- subset(comp.dat, select = c(Year, pink_numbers_np))
+names(comp.dat) <- c("Year", "pink_raw")
+
+
+## Fit time-trend models
+pink.lm <- lm(pink_raw ~ Year, data = comp.dat)
+
+pink.exp <- nls(pink_raw ~ I(exp(1)^(a + b * Year)),
+               data = comp.dat, start = c(a = 0, b = 0.001), trace = FALSE)
+
+pink.gam <- mgcv::gam(pink_raw ~ s(Year, k = 3), data = comp.dat, gamma = 2)
+
+
+## Save de-trended data
+comp.dat$pink_lm  <- residuals(pink.lm)
+comp.dat$pink_exp <- residuals(pink.exp)
+comp.dat$pink_gam <- residuals(pink.gam)
+write.csv(comp.dat, "./data/pink_abundance_detrend.csv", row.names = FALSE)
+
+
+## MSE
+sum(comp.dat$pink_lm^2)
+sum(comp.dat$pink_exp^2)
+sum(comp.dat$pink_gam^2)
+
+
+## Vis model fits
+pdf("./figures/detrend-covars/pink_fits.pdf", width = 8, height = 9)
+par(mfrow = c(2, 1))
+plot(comp.dat$Year, comp.dat$pink_raw,
+     type = "o",
+     lty = 2,
+     pch = 19,
+     xlab = "Year",
+     ylab = "Pink salmon abundance",
+     axes = FALSE)
+box(col = "grey50")
+axis(side = 1, lwd = 0, lwd.tick = 1, col = "grey65")
+axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey65")
+lines(comp.dat$Year, predict(pink.lm), lwd = 2, col = "blue3")
+lines(comp.dat$Year, predict(pink.exp),lwd = 2,  col = "red3")
+lines(comp.dat$Year, predict(pink.gam),lwd = 2,  col = "green3")
+legend("topleft", legend = c("data", "lm", "exp", "gam"), lwd = 2,
+       col = c("black", "blue3", "red3", "green3"),
+       bty = 'n')
+##
+plot(comp.dat$Year, resid(pink.lm),
+     ylim = c(-200, 200),
+     xlab = "Year",
+     ylab = "Residual pink index",
+     axes = FALSE,
+     type = "o",
+     pch = 19,
+     col = "blue3",
+     panel.first = abline(h = 0, col = "grey50", lty = 2))
+box(col = "grey50")
+axis(side = 1, lwd = 0, lwd.tick = 1, col = "grey65")
+axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey65")
+lines(comp.dat$Year, resid(pink.exp), type = "o", col = "red3", pch = 19)
+lines(comp.dat$Year, resid(pink.gam), type = "o", col = "green3", pch = 19)
+dev.off()
+
+
+## Vis residual series correlations
+pdf("./figures/detrend-covars/pink_cor.pdf", width = 8, height = 8)
+g <- splom(comp.dat[ , names(comp.dat) != "Year"],
+           par.settings = theme.mjm(),
+           pch = 19, las = 1,
+           panel = function(x, y, ...) {
+               panel.splom(x, y, ...)
+                panel.text(min(x), max(y),
+                           paste("r =", round(cor(x, y, use = "pairwise.complete.obs"), 2)),
+                           cex = 0.8, adj = 0, col = "black")
+           })
+print(g)
+dev.off()
+
+
+## Vis ACF of residual series
+pdf("./figures/detrend-covars/pink_acf.pdf", width = 9, height = 8)
+par(mfrow = c(2, 2), las = 1)
+acf(comp.dat$pink_raw, main = "Pink data")
+acf(resid(pink.lm), main = "Linear model residuals")
+acf(resid(pink.exp), main = "Exponential model residuals")
+acf(resid(pink.gam), main = "GAM model residuals")
+dev.off()
+
+# acf(comp.dat$pink_raw, main = "Pink data")$acf
+# acf(resid(pink.lm), main = "Linear model residuals")$acf
+# acf(resid(pink.exp), main = "Exponential model residuals")$acf
+# acf(resid(pink.gam), main = "GAM model residuals")$acf
+
+
+
+## SST 1st year --------------------------------------------
+## Here, I use the raw SST values, rather than the anomalies
+
+## Get SST data
+sst.dat <- read.table("./data/sst_yr_1_stock_anomalies.csv",
+                      stringsAsFactors = FALSE, sep = ",",
+                      header = TRUE)
+sst.dat <- sst.dat %>% filter(Stock.ID %in% info_master$Stock.ID)
+
+## Fit models
+sst.lst  <- vector("list", length(unique(sst.dat$Stock.ID)))
+sst.pred <- vector("list", length(unique(sst.dat$Stock.ID)))
+ind <- 1
+for(i in unique(sst.dat$Stock.ID)) {
+    sst.dat.i  <- sst.dat[sst.dat$Stock.ID == i, ]
+    sst.pred.i <- sst.dat[sst.dat$Stock.ID == i, ]
+    sst.lm <- lm(sst_raw ~ Year, data = sst.dat.i)
+    sst.exp <- nls(sst_raw ~ I(exp(1)^(a + b * Year)),
+                   control = list(maxiter = 500),
+                   data = sst.dat.i, start = c(a = -1, b = 0.001), trace = FALSE)
+    sst.gam <- mgcv::gam(sst_raw ~ s(Year, k = 3), data = sst.dat.i, gamma = 2)
+
+    sst.pred.i$sst_lm  <- predict(sst.lm)
+    sst.pred.i$sst_exp <- predict(sst.exp)
+    sst.pred.i$sst_gam <- predict(sst.gam)
+    sst.pred[[ind]] <- sst.pred.i
+
+    sst.dat.i$sst_lm  <- residuals(sst.lm)
+    sst.dat.i$sst_exp <- residuals(sst.exp)
+    sst.dat.i$sst_gam <- residuals(sst.gam)
+    sst.lst[[ind]] <- sst.dat.i
+    ind <- ind + 1
+}
+sst.detrend <- plyr::rbind.fill(sst.lst)
+sst.predict <- plyr::rbind.fill(sst.pred)
+write.csv(sst.detrend, "./data/sst_yr_1_stock_anomalies_detrend.csv", row.names = FALSE)
+
+
+## Vis model fits
+pdf("./figures/detrend-covars/sst_fits.pdf", width = 8, height = 9)
+for(i in unique(sst.detrend$Stock.ID)) {
+    sst.i <- sst.detrend[sst.detrend$Stock.ID == i, ]
+    pred.i <- sst.predict[sst.predict$Stock.ID == i, ]
+    par(mfrow = c(2, 1))
+    plot(sst.i$Year, sst.i$sst_raw,
+         type = "o",
+         main = i,
+         lty = 2,
+         pch = 19,
+         xlab = "Year",
+         ylab = "SST raw",
+         axes = FALSE,
+         panel.first = abline(h = 0, col = "grey50", lty = 2))
+    box(col = "grey50")
+    axis(side = 1, lwd = 0, lwd.tick = 1, col = "grey65")
+    axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey65")
+    lines(pred.i$Year, pred.i$sst_lm, lwd = 2, col = "blue3")
+    lines(pred.i$Year, pred.i$sst_exp, lwd = 2,  col = "red3")
+    lines(pred.i$Year, pred.i$sst_gam, lwd = 2,  col = "green3")
+    legend("topleft", legend = c("data", "lm", "exp", "gam"), lwd = 2,
+           col = c("black", "blue3", "red3", "green3"),
+           bty = 'n')
+    ##
+    plot(sst.i$Year, sst.i$sst_lm,
+         ylim = c(-2, 2),
+         xlab = "Year",
+         ylab = "Residual sst index",
+         axes = FALSE,
+         type = "o",
+         pch = 19,
+         col = "blue3",
+         panel.first = abline(h = 0, col = "grey50", lty = 2))
+    box(col = "grey50")
+    axis(side = 1, lwd = 0, lwd.tick = 1, col = "grey65")
+    axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey65")
+    lines(sst.i$Year, sst.i$sst_exp, type = "o", col = "red3", pch = 19)
+    lines(sst.i$Year, sst.i$sst_gam, type = "o", col = "green3", pch = 19)
+}
+dev.off()
+
+
+## Vis residual series correlations
+pdf("./figures/detrend-covars/sst_cor.pdf", width = 8, height = 8)
+for(i in unique(sst.detrend$Stock.ID)) {
+    sst.i <- sst.detrend[sst.detrend$Stock.ID == i, ]
+    g <- splom(sst.i[ , !names(sst.i) %in% c("Year", "sst_anomaly", "Stock.ID", "Species")],
+               par.settings = theme.mjm(),
+               main = as.character(i),
+               pch = 19, las = 1,
+               panel = function(x, y, ...) {
+                   panel.splom(x, y, ...)
+                    panel.text(min(x), max(y),
+                               paste("r =", round(cor(x, y, use = "pairwise.complete.obs"), 2)),
+                               cex = 0.8, adj = 0, col = "black")
+               })
+    print(g)
+}
+dev.off()
+
+
+## Vis ACF of residual series
+pdf("./figures/detrend-covars/sst_acf.pdf", width = 9, height = 8)
+for(i in unique(sst.detrend$Stock.ID)) {
+    sst.i <- sst.detrend[sst.detrend$Stock.ID == i, ]
+    par(mfrow = c(2, 2), las = 1)
+    acf(sst.i$sst_raw, main = "SST raw")
+    acf(sst.i$sst_lm, main = "Linear model residuals")
+    acf(sst.i$sst_exp, main = "Exponential model residuals")
+    acf(sst.i$sst_gam, main = "GAM model residuals")
+    title(as.character(i), line = -1.5, outer = TRUE)
+}
+dev.off()
+
+
+
+#### Join covariate data ####
+
+## SST during early marine life 
+sst.dt <- clim.wgt.avg(brood.table = bt.complete,
+                          env.data = sst.detrend,
+                          env.covar = "sst_lm",
+                          type = "first_year",
+                          out.covar = "early_sst_lm")
+
+
+## Age weighted competitor index: Pinks in 2nd yr marine life
+
+## competitors in second year of marine life
+comp.dt <- pink.wgt.avg(brood.table = bt.complete,
+                            pink.data = comp.dat,
+                            pink.covar = "pink_gam",
+                            type = "second_year",
+                            out.covar = "np_pinks_gam_sec")
+
+
+## Merge datasets 
+sock.detrend <- dplyr::left_join(bt.complete, sst.dt, by=c("BY","Stock.ID"))
+sock.detrend <- dplyr::left_join(sock.detrend, comp.dt, by=c("BY","Stock.ID"))
+sock.detrend <- geographic.order(sock.detrend) 
+
+
+## Add derived columns
+sock.detrend <- ddply(sock.detrend, .(Stock), transform,
+                      RS = R/S,
+                      RS_stnd = scale(R/S)[ , 1],
+                      lnRS = log(R/S),
+                      S_stnd = scale(S)[ , 1],
+                      early_sst_lm_stnd = scale(early_sst_lm)[ , 1],
+                      np_pinks_gam_sec_stnd = scale(np_pinks_gam_sec)[ , 1])
+
+
+#### Fit HBMs with detrended data ####
+## stationary model detrended ---------------------------------------
+
+## Run MCMC
+stan.dat <- stan_data_stat(sock.detrend,
+                          scale.x1 = TRUE,
+                          var.x2 = "early_sst_lm_stnd", # use lm detrend for sst
+                          var.x3 = "np_pinks_gam_sec_stnd", # use gam detrend for comp
+                          var.region = "Ocean.Region2",
+                          alpha.group = ifelse(speciesFlag=="sockeye", TRUE, FALSE)) 
+stat_detrend <- rstan::stan(file = "./stan/hbm_stat_2c.stan",
+                           data = stan.dat,
+                           warmup = 1000,
+                           iter = 3000,
+                           cores = 4,
+                           chains = 4,
+                           thin = 2,
+                           seed = 123,
+                           control = list(adapt_delta = 0.90,
+                                          max_treedepth = 10))
+save(stat_detrend, file = "./output/models/stat_detrend.RData")
+
+
+
+
+## eras model detrended -----------------------------------------------
+## Run MCMC
+stan.dat <- stan_data_dyn(sock.detrend,
+                          breakpoint2 = 2011,
+                          scale.x1 = TRUE,
+                          var.x2 = "early_sst_lm_stnd", # use lm detrend for sst
+                          var.x3 = "np_pinks_gam_sec_stnd") # use gam detrend for comp
+era_detrend <- rstan::stan(file = "./stan/hbm_era_2c.stan",
+                     data = stan.dat,
+                     warmup = 1000,
+                     iter = 3000,
+                     cores = 4,
+                     chains = 4,
+                     thin = 2,
+                     seed = 123,
+                     control = list(adapt_delta = 0.90,
+                                    max_treedepth = 10))
+save(era_detrend, file = "./output/models/era_detrend.RData")
+
+# Density dataframes - detrended
+df.s.dt <- era_hb_param_df(era_detrend, par=c("gamma", "kappa"))
+df.reg.dt <- era_hb_param_df(era_detrend, par=c("gamma", "kappa"), mu=T, neras=3)
+
+
+# load *NOT DETRENDED* era model fits
+load(file="./output/models/dyn/sockeye/hbm_era_2c_3sub.RData", verbose=T)
+# Eras df by stock
+era.stk <- era_hb_param_df(era.2c.3sub, par=c("gamma", "kappa"))
+era.reg <- era_hb_param_df(era.2c.3sub, par=c("gamma", "kappa"), mu = TRUE)
+
+
+### --- Figures
+
+## Dot-and-whiskers to compare detrended to 'regular'
+ggplot(ocean_region_lab(df.reg.dt)) + 
+  geom_hline(yintercept = 0, color = "grey50", linetype = 2, linewidth = 0.25) +
+  geom_point(aes(y=reg_mean, x=era, col=ocean_region_lab), shape=18, size=3) +
+  geom_segment(aes(y=lower_10, yend=upper_90, x=era, xend=era, col=ocean_region_lab), 
+               linewidth=.8) +
+  geom_point(data=ocean_region_lab(era.reg), aes(y=reg_mean, x=era, col=ocean_region_lab), 
+              position=position_nudge(x=.1), shape=15, size=2) + 
+  geom_segment(data=ocean_region_lab(era.reg), aes(y=lower_10, yend=upper_90, x=era, xend=era, col=ocean_region_lab), 
+               position=position_nudge(x=.1), linewidth=.8) + # squares = original era estimates!
+  facet_grid(cols=vars(varnam), rows=vars(ocean_region_lab), scales="free_y") +
+  scale_colour_manual(values=col.region, guide="none") +
+  labs(x="Era", y="Covariate effect") +  theme_sleek()
+
+
+## Density plot
+ggplot(dens.df.s.dt) + 
+  geom_vline(xintercept = 0, color = "grey50", linetype = 2, linewidth = 0.25) +
+  geom_path(aes(x=x, y=dens, group=stock, col=ocean_region_lab), alpha=0.2) + 
+  geom_path(data=dens.df.reg.dt, aes(x=x, y=dens, col=ocean_region_lab), alpha=0.85, linewidth=1) +
+  scale_colour_manual(values=col.region) +
+  facet_grid(rows=vars(era), cols=vars(varnam)) + 
+  coord_cartesian(xlim=c(-1, 1)) +
+  theme_minimal() + labs(x="covariate effect", y="", col="Ocean Region") +
+  theme(axis.text.y=element_blank(), 
+        legend.position="bottom")
+pdf(here(fig.dir, "era_detrend_dens.pdf"))
+print(g)
+dev.off()
+
+## Detrended time series
+par(mfrow=c(2,1))
+ggplot(sock.detrend) + 
+  geom_line(data=sock, aes(x=BY, y=early_sst, group=Stock), col="grey50", alpha=0.5) +
+  geom_line(aes(x=BY, y=early_sst_lm, group=Stock), col="slategray4") +
+  theme_sleek()
+
+ggplot(sock.detrend) + 
+  geom_line(data=sock, aes(x=BY, y=np_pinks_sec, group=Stock), col="grey50", alpha=0.5) +
+  geom_line(aes(x=BY, y=np_pinks_gam_sec, group=Stock), col="slategray4") +
+  theme_sleek()
+
+
+
---
+++ nonlinear.R
@@ -0,0 +1,257 @@
+# Nonlinearities analysis
+
+# Set up
+data_master = sock
+info_master = sock.info
+fig.dir <- here('hannahland')
+
+raw.sst.index <- clim.wgt.avg(brood.table = sock,
+                              env.data = raw.clim.sock,
+                              env.covar = "sst_raw",
+                              type = "first_year",
+                              out.covar = "early_sst")
+
+cov.dat <- full_join(raw.sst.index, sock[,c("Stock.ID", "Ocean.Region2","early_sst_stnd", "np_pinks_sec", "BY", "lnRS")] , by=c("Stock.ID", "BY"))
+
+# Show the relationship between raw SST and stnd sst
+ggplot(cov.dat) + geom_point(aes(x=early_sst, y=early_sst_stnd, col=Ocean.Region2))
+
+# Plot SST density by region # more different than by era
+g <- ggplot(cov.dat) + 
+  geom_density(aes(x=early_sst, col=Ocean.Region2, fill=Ocean.Region2), alpha=0.1, linewidth=1) + 
+  scale_colour_manual(values=rev(unname(col.region)), aesthetics=c("colour", "fill")) + 
+  theme_sleek() + labs(x="SST Index (°C)", y="Density", col="Region", fill="Region")
+png(filename=here(fig.dir, 'sst_dens.png'), width=801*2, height=486*2, res=72*2)
+print(g)
+dev.off()
+
+
+
+# Load fit with no covariates
+load(file=here('output', 'models', 'stat', 'sockeye', 'stat_nocov.RData'), verbose=T)
+summ_nc <- rstan::summary(nocov, pars="yresid", probs=c(0.025, 0.5, 0.975))$summary
+
+# Also load residuals from fit with covariates
+load(file="./output/models/stat/sockeye/stat_sub.RData", verbose=T)
+summ_c <- rstan::summary(stat_sub, pars="yresid", probs=NULL)$summary
+
+# Bind with data
+cov.dat.resid <- cbind(cov.dat, summ_nc[,"mean"], summ_c[,"mean"])
+names(cov.dat.resid) <- c(names(cov.dat), "resids", "cov_resids")
+cov.dat.resid <- ocean_region_lab(cov.dat.resid)
+
+#### Residuals versus time ####
+
+# 1. Residuals from stationary model without covariates
+g <- ggplot(cov.dat.resid) + 
+    geom_point(aes(x=BY, y=resids, col=ocean_region_lab), alpha=0.2, shape=16) + 
+    hline_0(alpha=0.2) + geom_vline(xintercept=c(1989, 2011), alpha=0.2, lty="dashed") +
+    geom_smooth(aes(x=BY, y=resids, col=ocean_region_lab)) +
+    scale_colour_manual(values=col.region, guide="none") +
+    theme_sleek() + labs(x="Brood Year", y="Residuals", title="Residuals (no covariates) vs. time") + 
+    facet_wrap(vars(ocean_region_lab), scales = "free_y") 
+png(filename=here(fig.dir, 'no_cov_residsxtime.png'), width=801*2, height=486*2, res=72*3)
+print(g)
+dev.off()
+
+# 2. Residuals from stationary model with covariates
+g <- ggplot(cov.dat.resid) + 
+    geom_point(aes(x=BY, y=cov_resids, col=ocean_region_lab), alpha=0.2, shape=16) + 
+    hline_0(alpha=0.2) + geom_vline(xintercept=c(1989, 2011), alpha=0.2, lty="dashed") +
+    geom_smooth(aes(x=BY, y=resids), col="gray50", alpha=0.7, se=F) +
+    geom_smooth(aes(x=BY, y=cov_resids, col=ocean_region_lab)) +
+    scale_colour_manual(values=col.region, guide="none") +
+    theme_sleek() + labs(x="Brood Year", y="Residuals", title="Residuals (w/ covariates) vs. time") + 
+    facet_wrap(vars(ocean_region_lab), scales = "free_y")
+png(filename=here(fig.dir, 'cov_residsxtime.png'), width=801*2, height=486*2, res=72*3)
+print(g)
+dev.off()
+
+
+#### Residuals versus covariates ####
+
+# 1. Residuals vs SST by region
+g <- ggplot(cov.dat.resid) + geom_point(aes(x=early_sst, y=resids, col=ocean_region_lab), alpha=0.2, shape=16) +
+      hline_0(alpha=0.2) +
+      geom_smooth(aes(x=early_sst, y=resids, col=ocean_region_lab)) + 
+      theme_sleek() + labs(x="SST (degrees C)", y="Residuals", title="Residuals (no covariates) vs. SST") + 
+      scale_colour_manual(values=col.region, guide="none") +
+      facet_wrap(vars(ocean_region_lab), scales="free_y")
+png(filename=here(fig.dir, 'no_cov_residsxsst.png'), width=801*2, height=486*2, res=72*3)
+print(g)
+dev.off()
+
+# 2. Residuals vs SST - 'global'
+g <- ggplot(cov.dat.resid) + geom_point(aes(x=early_sst, y=resids, col=ocean_region_lab), alpha=0.2, shape=16) +
+      hline_0(alpha=0.2) +
+      geom_smooth(aes(x=early_sst, y=resids), col="grey50") +
+      theme_sleek() + labs(x="SST (degrees C)", y="Residuals", title="Residuals (no covariates) vs. SST", col="Region") + 
+      scale_colour_manual(values=col.region) 
+png(filename=here(fig.dir, 'no_cov_residsxsst_global.png'), width=801*2, height=486*2, res=72*3)
+print(g)
+dev.off()
+
+# 3. Residuals vs Pinks by region
+g <- ggplot(cov.dat.resid) + geom_point(aes(x=np_pinks_sec, y=resids, col=ocean_region_lab), alpha=0.2, shape=16) + 
+      hline_0(alpha=0.2) +
+      geom_smooth(aes(x=np_pinks_sec, y=resids, col=ocean_region_lab), alpha=0.5) +
+      theme_sleek() + labs(x="NP Pink Abundance", y="Residuals", title="Residuals (no covariates) vs. NP Pinks") + 
+      scale_colour_manual(values=col.region, guide="none") +
+      facet_wrap(vars(ocean_region_lab), scales="free_y") 
+png(filename=here(fig.dir, 'no_cov_residsxpinks.png'), width=801*2, height=486*2, res=72*3)
+print(g)
+dev.off()
+
+# 4. 2-D covariates (SST and pinks) with residual size
+ggplot(cov.dat.resid) + 
+      geom_point(aes(x=early_sst, y=BY, col=resids, size=abs(resids)), alpha=0.7) + 
+      theme_sleek() +
+      guides(size="none") +
+      scale_colour_distiller(palette = "RdBu") +
+      labs(x="SST (degrees C)", y="NP Pink abundance", title="Residuals vs. covariates", col="Residual", size="Residual")  
+png(filename=here(fig.dir, 'residsxcovars.png'), width=801*2, height=486*2, res=72*3)
+print(g)
+dev.off()
+
+
+#### Resids vs covars by era ####
+
+# Fitting a simple lm for the lines - NOT hbm estimated rels.
+g <- cov.dat.resid %>%
+  mutate(era = case_when(BY<1989 ~ "Early",
+                          BY >= 1989 & BY <2011 ~ "Middle",
+                          BY >= 2011 ~ "Late")) %>%
+  mutate(regxera = paste(ocean_region_lab, era, sep=".")) %>%
+  ggplot() + hline_0(col="gray70") + 
+  geom_point(aes(x=early_sst, y=resids, col=factor(regxera, levels=unique(regxera))), alpha=0.2, shape=16) +
+  geom_smooth(aes(x=early_sst, y=resids, col=factor(regxera, levels=unique(regxera))), method="lm", se=F) +
+  scale_colour_manual(values=col.eras) +
+  facet_wrap(vars(ocean_region_lab), scales="free_y") +
+  labs(x="SST (degrees C)", y="Residuals", col="Region x Era") +
+  theme_sleek()
+png(filename=here(fig.dir, 'eras_residsxsst.png'), width=801*2, height=486*2, res=72*3)
+print(g)
+dev.off()
+
+g <- cov.dat.resid %>%
+  mutate(era = case_when(BY<1989 ~ "Early",
+                         BY >= 1989 & BY <2011 ~ "Middle",
+                         BY >= 2011 ~ "Late")) %>%
+  mutate(regxera = paste(ocean_region_lab, era, sep=".")) %>%
+  ggplot() + hline_0(col="gray70") + 
+  geom_point(aes(x=np_pinks_sec, y=resids, col=factor(regxera, levels=unique(regxera))), alpha=0.2, shape=16) +
+  geom_smooth(aes(x=np_pinks_sec, y=resids, col=factor(regxera, levels=unique(regxera))), method="lm", se=F) +
+  scale_colour_manual(values=col.eras) +
+  facet_wrap(vars(ocean_region_lab), scales="free_y") +
+  labs(x="NP Pink abundance", y="Residuals", col="Region x Era") +
+  theme_sleek()
+png(filename=here(fig.dir, 'eras_residsxcomp.png'), width=801*2, height=486*2, res=72*3)
+print(g)
+dev.off()
+
+
+#### Detrended covariates ####
+
+#source("detrend_covars.R")
+
+# Load detrended model fit
+load(file = "./output/models/stat_detrend.RData", verbose=T)
+df.reg.dt <- era_hb_param_df(era_detrend, par=c("gamma", "kappa"), mu=T, neras=3)
+
+
+# load *not detrended* era model fits
+load(file="./output/models/dyn/sockeye/hbm_era_2c_3sub.RData", verbose=T)
+era.reg <- era_hb_param_df(era.2c.3sub, par=c("gamma", "kappa"), mu = TRUE)
+
+### --- Figures
+
+## Dot-and-whiskers to compare detrended to not detrended era model
+g <- ggplot(ocean_region_lab(df.reg.dt)) + 
+  geom_hline(yintercept = 0, color = "grey50", linetype = 2, linewidth = 0.25) +
+  geom_point(aes(y=reg_mean, x=era, col=ocean_region_lab), shape=18, size=3) +
+  geom_segment(aes(y=lower_10, yend=upper_90, x=era, xend=era, col=ocean_region_lab), 
+               linewidth=.8) +
+  geom_point(data=ocean_region_lab(era.reg), aes(y=reg_mean, x=era, col=ocean_region_lab), 
+             position=position_nudge(x=.1), shape=15, size=2) + 
+  geom_segment(data=ocean_region_lab(era.reg), aes(y=lower_10, yend=upper_90, x=era, xend=era, col=ocean_region_lab), 
+               position=position_nudge(x=.1), linewidth=.8) + # squares = original era estimates!
+  facet_grid(cols=vars(varnam), rows=vars(ocean_region_lab), scales="free_y") +
+  scale_colour_manual(values=col.region, guide="none") +
+  labs(x="Era", y="Covariate effect") +  theme_sleek()
+png(filename=here(fig.dir, 'eras_detrend_compare.png'), width=600*2, height=500*2, res=72*3)
+print(g)
+dev.off()
+
+
+
+# Get detrended model residuals
+summ_dt <- rstan::summary(stat_detrend, pars="yresid", probs=NULL)$summary
+
+# Bind with data
+detrend.resid <- cbind(sock.detrend, summ_dt[,"mean"])
+names(detrend.resid) <- c(names(detrend.resid)[1:(ncol(detrend.resid)-1)], "cov_resids")
+detrend.resid <- ocean_region_lab(detrend.resid)
+
+### Plot detrended covariates
+ggplot(detrend.resid) + geom_line(aes(x=BY, y=np_pinks_gam_sec, group=Stock)) + 
+  facet_wrap(vars(ocean_region_lab))
+
+
+#### Residuals versus time ####
+
+g <- ggplot(detrend.resid) + 
+  geom_point(aes(x=BY, y=cov_resids, col=ocean_region_lab), alpha=0.2, shape=16) + 
+  hline_0(alpha=0.2) + geom_vline(xintercept=c(1989, 2011), alpha=0.2, lty="dashed") +
+  geom_smooth(aes(x=BY, y=cov_resids, col=ocean_region_lab)) +
+  scale_colour_manual(values=col.region, guide="none") +
+  theme_sleek() + labs(x="Brood Year", y="Residuals", title="Residuals vs. time (detrended covars)") + 
+  facet_wrap(vars(ocean_region_lab), scales = "free_y") 
+png(filename=here(fig.dir, 'detrended_cov_residsxtime.png'), width=801*2, height=486*2, res=72*3)
+print(g)
+dev.off()
+
+#### Residuals vs covariates ####
+
+# 1. Residuals vs SST by region
+g <- ggplot(detrend.resid) + geom_point(aes(x=early_sst_lm, y=cov_resids, col=ocean_region_lab), alpha=0.2, shape=16) +
+  hline_0(alpha=0.2) +
+  geom_smooth(aes(x=early_sst_lm, y=cov_resids, col=ocean_region_lab)) +
+  theme_sleek() + labs(x="Detrended SST", y="Residuals", title="Residuals (detrended covariates) vs. SST") + 
+  scale_colour_manual(values=col.region, guide="none") +
+  facet_wrap(vars(ocean_region_lab), scales="free_y")
+png(filename=here(fig.dir, 'detrended_cov_residsxsst.png'), width=801*2, height=486*2, res=72*3)
+print(g)
+dev.off()
+
+# 2. Residuals vs SST - 'global' # NA for now; unsure if matching to 'raw' sst is appropriate
+
+# 3. Residuals vs Pinks by region
+g <- ggplot(detrend.resid) + geom_point(aes(x=np_pinks_gam_sec, y=cov_resids, col=ocean_region_lab), alpha=0.2, shape=16) + 
+  hline_0(alpha=0.2) +
+  geom_smooth(aes(x=np_pinks_gam_sec, y=cov_resids, col=ocean_region_lab), alpha=0.5) +
+  theme_sleek() + labs(x="Detrended NP Pink Abundance", y="Residuals", title="Residuals (detrended covariates) vs. NP Pinks") + 
+  scale_colour_manual(values=col.region, guide="none") +
+  facet_wrap(vars(ocean_region_lab), scales="free_y") 
+png(filename=here(fig.dir, 'detrended_cov_residsxcomp.png'), width=801*2, height=486*2, res=72*3)
+print(g)
+dev.off()
+
+#### Detrended eras model ####
+load( file = "./output/models/era_detrend.RData", verbose=T)
+summ_era_dt <- rstan::summary(era_detrend, pars="yresid", probs=NULL)$summary
+
+# Bind with data
+detrend.resid.era <- cbind(sock.detrend, summ_era_dt[,"mean"])
+names(detrend.resid.era) <- c(names(detrend.resid.era)[1:(ncol(detrend.resid.era)-1)], "cov_resids")
+detrend.resid.era <- ocean_region_lab(detrend.resid.era)
+
+# Residuals over time
+ggplot(detrend.resid.era) + 
+  geom_point(aes(x=BY, y=cov_resids, col=ocean_region_lab), alpha=0.2, shape=16) + 
+  hline_0(alpha=0.2) + geom_vline(xintercept=c(1989, 2011), alpha=0.2, lty="dashed") +
+  geom_smooth(aes(x=BY, y=cov_resids, col=ocean_region_lab)) +
+  scale_colour_manual(values=col.region, guide="none") +
+  theme_sleek() + labs(x="Brood Year", y="Residuals", title="Residuals vs. time (detrended covars)") + 
+  facet_wrap(vars(ocean_region_lab), scales = "free_y")
+
+
