690be948ba31f5d2a5422e986ba762f4f5e04111
Connors <Brendan.Connors@dfo-mpo.gc.ca>
2026-04-15 17:02:12 -0700

    add detrended pinks to chum and pink covariate tables

--- pink_chum_covariates.R
+++ pink_chum_covariates.R
@@ -22,6 +22,9 @@ raw.clim.pink <- filter(raw.clim, Species=="Pink")
 bt.raw.pink <- read.csv("./data/pink/master_pink_brood_table.csv", header=T)
 bt.complete.pink <- bt.raw.pink[complete.cases(bt.raw.pink),]
 
+## Add de-trended north pacific pinks
+pink_gam <- gam(pink_numbers_np ~ s(Year), data = raw.comp)
+raw.comp$pink_detrend_np <- residuals(pink_gam)
 
 ## Climate index: SST at ocean entry point in BY+1
 ## SST during early marine life : no age weighting required
@@ -31,12 +34,12 @@ early.sst.pink <- dplyr::left_join(bt.complete.pink, raw.clim.pink[,names(raw.cl
 ## competitor index: Pink salmon NP abundance
 ## competitors in BY+2
 raw.comp$BY <- raw.comp$Year - 2
-np.pink.sec.pink <- dplyr::left_join(bt.complete.pink[,c("Stock.ID", "BY", "Species")], raw.comp[, c("all_spp_numbers_np", "pink_numbers_np", "BY")], by= "BY")
+np.pink.sec.pink <- dplyr::left_join(bt.complete.pink[,c("Stock.ID", "BY", "Species")], raw.comp[, c("all_spp_numbers_np", "pink_numbers_np", "pink_detrend_np","BY")], by= "BY")
 
 
 ## Merge datasets
 master.pink <- dplyr::left_join(early.sst.pink, np.pink.sec.pink, by=c("BY","Stock.ID", "Species"))
-master.pink.bt_w_cov1 <- dplyr::rename(master.pink, early_sst = sst_anomaly, np_pinks_sec = pink_numbers_np, np_all_spp_sec = all_spp_numbers_np)
+master.pink.bt_w_cov1 <- dplyr::rename(master.pink, early_sst = sst_anomaly, np_pinks_sec = pink_numbers_np, np_all_spp_sec = all_spp_numbers_np, det_np_pinks_sec = pink_detrend_np)
 master.pink.bt_w_cov1 <- geographic.order(master.pink.bt_w_cov1) # Ordered factor
 head(master.pink.bt_w_cov1)
 summary(master.pink.bt_w_cov1)
@@ -51,7 +54,8 @@ master.pink.bt_w_cov2 <- ddply(master.pink.bt_w_cov1, .(Stock), transform,
                           S_stnd = scale(S)[ , 1],
                           early_sst_stnd = scale(early_sst)[ , 1],
                           np_all_spp_sec_stnd = scale(np_all_spp_sec)[ , 1],
-                          np_pinks_sec_stnd = scale(np_pinks_sec)[ , 1])
+                          np_pinks_sec_stnd = scale(np_pinks_sec)[ , 1],
+                          det_np_pinks_sec_stnd = scale(det_np_pinks_sec)[ , 1])
 
 
 # Export to output
@@ -85,12 +89,11 @@ early.sst.chum <- dplyr::left_join(bt.complete.chum, raw.clim.chum[,names(raw.cl
 ## competitor index: pinks in BY+2
 ## competitors in second year of marine life
 raw.comp$BY <- raw.comp$Year - 2
-np.pink.sec.chum <- dplyr::left_join(bt.complete.chum[,c("Stock.ID", "BY", "Species")], raw.comp[, c("pink_numbers_np", "BY")], by= "BY")
-
+np.pink.sec.chum <- dplyr::left_join(bt.complete.chum[,c("Stock.ID", "BY", "Species")], raw.comp[, c("pink_numbers_np", "pink_detrend_np", "BY")], by= "BY")
 
 ## Merge datasets
 master.chum <- dplyr::left_join(early.sst.chum, np.pink.sec.chum, by=c("BY","Stock.ID", "Species"))
-master.chum.bt_w_cov1 <- dplyr::rename(master.chum, early_sst = sst_anomaly, np_pinks_sec = pink_numbers_np)
+master.chum.bt_w_cov1 <- dplyr::rename(master.chum, early_sst = sst_anomaly, np_pinks_sec = pink_numbers_np, det_np_pinks_sec = pink_detrend_np)
 master.chum.bt_w_cov1 <- geographic.order(master.chum.bt_w_cov1) # Ordered factor
 head(master.chum.bt_w_cov1)
 summary(master.chum.bt_w_cov1)
@@ -104,7 +107,8 @@ master.chum.bt_w_cov2 <- ddply(master.chum.bt_w_cov1, .(Stock), transform,
                                lnRS = log(R/S),
                                S_stnd = scale(S)[ , 1],
                                early_sst_stnd = scale(early_sst)[ , 1],
-                               np_pinks_sec_stnd = scale(np_pinks_sec)[ , 1])
+                               np_pinks_sec_stnd = scale(np_pinks_sec)[ , 1],
+                               det_np_pinks_sec_stnd = scale(det_np_pinks_sec)[ , 1])
 
 
 # Export to output
